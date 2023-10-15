a32bce7244f799424aaf843b1ee01d2cd6b70f13
William Almeida <william.almeida@westpoint.io>
2023-10-15 18:06:24 -0300

    feat: adapts ui to use grid and reworks tasks data

--- task_scheduler/task_scheduler_ui.py
+++ task_scheduler/task_scheduler_ui.py
@@ -7,14 +7,13 @@ import datetime
 
 tasks_file_path = 'tasks.json'
 task_time_delay_types = ['Seconds', 'Minutes', 'Hours', 'Days', 'Weeks', 'Months', 'Years']
-task_time_option_types = ['By Delay', 'By Date and time']
+task_time_option_types = ['Delay', 'Date and time']
 task_month_options = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 screen = 0
 root = tk.Tk()
 root.title("Task Scheduler")
-time_label = tk.Label(root, font=("Helvetica", 16))
-# time_label.pack(padx=20, pady=20)
+time_label = tk.Label(root, font=("Courier", 14))
 
 def read_file_data(path):
   if(not os.path.exists(path)):
@@ -68,33 +67,33 @@ def get_date_after_delay(delay_answers):
 def get_tasks_remaining_time(file_data):
   now = datetime.datetime.now()
   data = ''
+  biggest_name = None
   for task in file_data:
     task_parsed_date = datetime.datetime.strptime(task['date'], '%Y-%m-%d %H:%M:%S')
     task_remaining_time = task_parsed_date - now
     overdue = task_remaining_time < datetime.timedelta(0)
     task['remaining_time_to_alert'] = datetime.timedelta(0) if overdue else task_remaining_time
-
+    if not biggest_name or len(task['name']) > len(biggest_name):
+      biggest_name = task['name']
   ordered_tasks = sorted(file_data, key=lambda task: task['remaining_time_to_alert'])
-
+  data += "\nTask Name" + " " * (len(biggest_name) - len("Task Name")) + ' ---------- ' + "Remaining Time" +'\n'
   for task in ordered_tasks:
     remaining_days = task['remaining_time_to_alert'].days
     remaining_hours, remainder = divmod(task['remaining_time_to_alert'].seconds, 3600)
     remaining_minutes, _ = divmod(remainder, 60)
     remaining_seconds =task['remaining_time_to_alert'].seconds % 60
     time_expired = remaining_days <= 0 and remaining_hours <= 0 and remaining_minutes <= 0 and remaining_seconds <= 0
-    remaining_time = "Time Expired" if time_expired else f"{remaining_days} day(s), {remaining_hours} hour(s) and {remaining_minutes} minute(s) and {remaining_seconds} seconds"
-
-    name_spacing = 20 - len(task['name'])
-    time_spacing = 20 - len(remaining_time)
-
-    data += task['name']+" " + '-' * name_spacing + '-' * time_spacing + remaining_time +'\n'
+    remaining_time = "Time Expired" if time_expired else f"{remaining_days}d {remaining_hours}h {remaining_minutes}m {remaining_seconds}s"
+    name_spaces = len(biggest_name) - len(task['name'])
+    print(task['name'], (str(name_spaces)))
+    data += task['name']+" " * name_spaces + ' ---------- ' + remaining_time +'\n'
   return data
 
 def handle_delay_answer():
     name = task_name.get()
     delay_type_value = delay_type.get()
     delay_quantity_value = delay_quantity.get()
-    print(name, delay_type_value, delay_quantity_value)
+
     delay_answers = {
       'delay_type': delay_type_value,
       'delay_multiplier': delay_quantity_value
@@ -108,7 +107,8 @@ def handle_delay_answer():
     print("We will remind you about task: '"+name+"' in " +
       delay_answers['delay_multiplier'] + " " + delay_type_value + " ("+str(to_remind_date)+")"
     )
-    return new_task
+    handle_back()
+    clear_delay_task_input_values()
 
 def handle_date_time_answer():
     name = task_name.get()
@@ -142,174 +142,244 @@ def handle_date_time_answer():
     print("We will remind you about task: '"+name+"' in " +
        str(to_remind_date)
     )
-    return new_task
+    handle_back()
+    clear_date_time_task_input_values()
 
-def submit(schedule_type):
-    print('SUBMIT')
-    # TODO validate inputs
-    # TODO show message, wait 5 secs, remove elements and go back to first screen
-    if(schedule_type == 'delay'):
-      handle_delay_answer()
-    elif(schedule_type == 'date_time'):
-      handle_date_time_answer()
+def validate_date_time_answer():
+    name = task_name.get()
+    year = date_time_year.get()
+    month = date_time_month.get()
+    day = date_time_day.get()
+    hour = date_time_minutes.get()
+    minute = date_time_minutes.get()
+    second = date_time_seconds.get()
+    if not name or not year or not month or not day or not hour or not minute or not second:
+      error_message.config(text="Please fill all fields")
+      error_message.grid(row=11, column=0, columnspan=2)
+      return False
+    return True
+
+def validate_delay_answer():
+    name = task_name.get()
+    delay_type_value = delay_type.get()
+    delay_quantity_value = delay_quantity.get()
+    if not name or not delay_type_value or not delay_quantity_value:
+      error_message.config(text="Please fill all fields")
+      error_message.grid(row=8, column=0, columnspan=2)
+      return False
+    return True
+
+def submit():
+    schedule_type_value = schedule_type.get()
+    error_message.config(text="")
+    print('SUBMIT', schedule_type_value)
+    if(schedule_type_value == 'Delay'):
+      is_answer_valid = validate_delay_answer()
+      print(is_answer_valid)
+      if is_answer_valid:
+        handle_delay_answer()
+        # TODO show message, wait 5 secs, remove elements and go back to first screen
+
+    elif(schedule_type_value == 'Date and time'):
+      is_answer_valid = validate_date_time_answer()
+      if is_answer_valid:
+        handle_date_time_answer()
+        # TODO show message, wait 5 secs, remove elements and go back to first screen
 
 
 # Schedule task Delay type
-delay_label = tk.Label(root, text="Delay type task:")
-delay_type_label = tk.Label(root, text="Select a delay type")
+delay_label = tk.Label(root, text="Delay type task: ")
+delay_type_label = tk.Label(root, text="Select a delay type: ")
 delay_type = ttk.Combobox(root, values=task_time_delay_types)
-delay_quantity_label = tk.Label(root, text="Insert a delay quantity")
+delay_quantity_label = tk.Label(root, text="Insert a delay quantity: ")
 delay_quantity = tk.Entry(root)
-delay_submit_button = tk.Button(root, text="Submit", command=lambda: submit('delay'))
+delay_submit_button = tk.Button(root, text="Submit", command=lambda: submit())
+
+
+def clear_delay_task_input_values():
+  task_name.delete(0, tk.END)
+  schedule_type.delete(0, tk.END)
+  delay_type.delete(0, tk.END)
+  delay_quantity.delete(0, tk.END)
 
 def clear_delay_task_elements():
-  delay_label.pack_forget()
-  delay_type_label.pack_forget()
-  delay_type.pack_forget()
-  delay_quantity_label.pack_forget()
-  delay_quantity.pack_forget()
-  delay_submit_button.pack_forget()
+  delay_label.grid_remove()
+  delay_type_label.grid_remove()
+  delay_type.grid_remove()
+  delay_quantity_label.grid_remove()
+  delay_quantity.grid_remove()
+  delay_submit_button.grid_remove()
 
 def schedule_delay_task():
   clear_date_time_task_elements()
-  delay_label.pack()
-  delay_type_label.pack()
-  delay_type.pack()
-  delay_quantity_label.pack()
-  delay_quantity.pack()
-  delay_submit_button.pack()
+  delay_label.grid(row=4, column=0, columnspan=2)
+  delay_type_label.grid(row=5, column=0)
+  delay_type.grid(row=5, column=1)
+  delay_quantity_label.grid(row=6, column=0)
+  delay_quantity.grid(row=6, column=1)
+  back_button.grid(row=7, column=0)
+  delay_submit_button.grid(row=7, column=1)
 
 # Schedule task Date and time type
 date_time_label = tk.Label(root, text="Date and time task:")
-date_time_year_label = tk.Label(root, text="Year")
+date_time_year_label = tk.Label(root, text="Year: ")
 date_time_year = tk.Entry(root)
-date_time_month_label = tk.Label(root, text="Select a month")
+date_time_month_label = tk.Label(root, text="Month: ")
 date_time_month = ttk.Combobox(root, values=task_month_options)
-date_time_day_label = tk.Label(root, text="Day")
+date_time_day_label = tk.Label(root, text="Day: ")
 date_time_day = tk.Entry(root)
-date_time_minutes_label = tk.Label(root, text="Minutes")
+date_time_minutes_label = tk.Label(root, text="Minutes: ")
 date_time_minutes = tk.Entry(root)
-date_time_seconds_label = tk.Label(root, text="Seconds")
+date_time_seconds_label = tk.Label(root, text="Seconds: ")
 date_time_seconds = tk.Entry(root)
-date_time_submit_button = tk.Button(root, text="Submit", command=lambda: submit('date_time'))
+date_time_submit_button = tk.Button(root, text="Submit", command=lambda: submit())
 
+
+def clear_date_time_task_input_values():
+  task_name.delete(0, tk.END)
+  schedule_type.delete(0, tk.END)
+  date_time_year.delete(0, tk.END)
+  date_time_month.delete(0, tk.END)
+  date_time_day.delete(0, tk.END)
+  date_time_minutes.delete(0, tk.END)
+  date_time_seconds.delete(0, tk.END)
 
 def clear_date_time_task_elements():
-  date_time_label.pack_forget()
-  date_time_year_label.pack_forget()
-  date_time_year.pack_forget()
-  date_time_month_label.pack_forget()
-  date_time_month.pack_forget()
-  date_time_day_label.pack_forget()
-  date_time_day.pack_forget()
-  date_time_minutes_label.pack_forget()
-  date_time_minutes.pack_forget()
-  date_time_seconds_label.pack_forget()
-  date_time_seconds.pack_forget()
-  date_time_submit_button.pack_forget()
+  date_time_label.grid_remove()
+  date_time_year_label.grid_remove()
+  date_time_year.grid_remove()
+  date_time_month_label.grid_remove()
+  date_time_month.grid_remove()
+  date_time_day_label.grid_remove()
+  date_time_day.grid_remove()
+  date_time_minutes_label.grid_remove()
+  date_time_minutes.grid_remove()
+  date_time_seconds_label.grid_remove()
+  date_time_seconds.grid_remove()
+  date_time_submit_button.grid_remove()
 
 def schedule_date_time_task():
   clear_delay_task_elements()
 
-  date_time_label.pack()
-  date_time_year_label.pack()
-  date_time_year.pack()
-  date_time_month_label.pack()
-  date_time_month.pack()
-  date_time_day_label.pack()
-  date_time_day.pack()
-  date_time_minutes_label.pack()
-  date_time_minutes.pack()
-  date_time_seconds_label.pack()
-  date_time_seconds.pack()
-  date_time_submit_button.pack()
+  date_time_label.grid(row=4, column=0, columnspan=2)
+  date_time_year_label.grid(row=5, column=0)
+  date_time_year.grid(row=5, column=1)
+  date_time_month_label.grid(row=6, column=0)
+  date_time_month.grid(row=6, column=1)
+  date_time_day_label.grid(row=7, column=0)
+  date_time_day.grid(row=7, column=1)
+  date_time_minutes_label.grid(row=8, column=0)
+  date_time_minutes.grid(row=8, column=1)
+  date_time_seconds_label.grid(row=9, column=0)
+  date_time_seconds.grid(row=9, column=1)
+  back_button.grid(row=10, column=0)
+  date_time_submit_button.grid(row=10, column=1)
+
+def handle_schedule_selection():
+  error_message.config(text="")
+  value = schedule_type.get()
+  if(value == 'Delay'):
+    schedule_delay_task()
+  elif(value == 'Date and time'):
+    schedule_date_time_task()
 
 # Task schedule initial elements
 task_label = tk.Label(root, text="Schedule a New Task:")
+task_name_label = tk.Label(root, text="Task name: ")
 task_name = tk.Entry(root)
-
-task_schedule_type_label = tk.Label(root, text="Select a option of schedule")
-schedule_type_delay = tk.Button(root, text="Delay", command=schedule_delay_task)
-schedule_type_date_time = tk.Button(root, text="Date and time", command=schedule_date_time_task)
+task_schedule_type_label = tk.Label(root, text="Select a option of schedule: ")
+schedule_type = ttk.Combobox(root, values=task_time_option_types)
+schedule_type.bind("<<ComboboxSelected>>", lambda e: handle_schedule_selection())
 
 def schedule_task():
-  task_label.pack()
-  task_name.pack()
-  task_schedule_type_label.pack()
-  schedule_type_delay.pack()
-  schedule_type_date_time.pack()
+  #  ????
+  follow_up_frame = tk.Frame(root)
+  follow_up_frame.grid(row=1, column=0, columnspan=2)
+
+  task_label.grid(row=0, column=0)
+  task_name_label.grid(row=1, column=0)
+  task_name.grid(row=1, column=1)
+  task_schedule_type_label.grid(row=2, column=0)
+  schedule_type.grid(row=2, column=1)
 
 def update_remaining_time():
     file_data = read_file_data(tasks_file_path)
     current_time = time.strftime("%H:%M:%S")
     tasks_data = get_tasks_remaining_time(file_data)
-    time_label.config(text=f"Current Time: {current_time}\n{tasks_data}")
-
-    root.after(1000, update_remaining_time)  # Update every 1000 milliseconds (1 second)
+    time_label.config(text=f"Current Time: {current_time}\n{tasks_data}", justify=tk.LEFT)
+    root.after(1000, update_remaining_time)
 
 def remove_show_tasks_elements():
-  time_label.pack_forget()
+  time_label.grid_remove()
 
 def remove_schedule_task_elements():
-  task_label.pack_forget()
-  task_name.pack_forget()
-  task_schedule_type_label.pack_forget()
-  schedule_type_delay.pack_forget()
-  schedule_type_date_time.pack_forget()
-  delay_type_label.pack_forget()
-  delay_type.pack_forget()
+  task_label.grid_remove()
+  task_name_label.grid_remove()
+  task_name.grid_remove()
+  task_schedule_type_label.grid_remove()
+  schedule_type.grid_remove()
+  delay_type_label.grid_remove()
+  delay_type.grid_remove()
 
 def handle_back():
-  print('back')
-
+  error_message.config(text="")
   remove_show_tasks_elements()
   remove_schedule_task_elements()
   clear_date_time_task_elements()
   clear_delay_task_elements()
+  back_button.grid_remove()
 
-  back_button.pack_forget()
-
-  main_label.pack()
-  add_task_button1.pack()
-  add_task_button2.pack()
-  exit_button.pack()
+  main_label.grid(row=0, column=0, columnspan=2)
+  show_tasks_button.grid(row=1, column=0)
+  schedule_task_button.grid(row=1, column=1)
+  exit_button.grid(row=2, column=0)
   # TODO
   #  remove root loop
-  # root.mainloop().destroy()
+  # root.mainloop().grid_remove()
 
 def handle_show_tasks():
-  handle_action_selection()
+  handle_action_selection('show_tasks')
   update_remaining_time()
-  time_label.pack()
+  time_label.grid(row=0, column=0, columnspan=2)
 
 def handle_schedule_task():
   print('schedule task')
-  handle_action_selection()
+  handle_action_selection('schedule_task')
   schedule_task()
-
-
 
 
 # Initial elements
 main_label = tk.Label(root, text="Task Time Tracker", font=("Helvetica", 16), padx=10, pady=5)
-add_task_button1 = tk.Button(root, text="Show tasks", command=handle_show_tasks)
-add_task_button2 = tk.Button(root, text="Schedule task", command=handle_schedule_task)
+show_tasks_button = tk.Button(root, text="Show tasks", command=handle_show_tasks)
+schedule_task_button = tk.Button(root, text="Schedule task", command=handle_schedule_task)
 back_button = tk.Button(root, text="Back", command=handle_back)
 exit_button = tk.Button(root, text="Exit", command=root.quit)
+error_message = tk.Label(root, text="", fg="red")
+
+main_label.grid(row=0, column=0, columnspan=2)
+show_tasks_button.grid(row=1, column=0)
+schedule_task_button.grid(row=1, column=1 )
+exit_button.grid(row=2, column=0, columnspan=2)
 
 
-main_label.pack()
-add_task_button1.pack()
-add_task_button2.pack()
-exit_button.pack()
+def mount_back_button(action):
 
-def handle_action_selection():
-   main_label.pack_forget()
-   add_task_button1.pack_forget()
-   add_task_button2.pack_forget()
-   exit_button.pack_forget()
-   if back_button.winfo_ismapped() == 0:
-     back_button.pack()
+   if back_button.winfo_ismapped() != 0:
+     return
+   if (action == 'show_tasks'):
+     back_button.grid(row=2, column=0, columnspan=2)
+   elif (action == 'schedule_task'):
+      # if not schedule selected
+      back_button.grid(row=4, column=0, columnspan=2)
+    # if schedule = delay
+
+def handle_action_selection(action):
+   main_label.grid_remove()
+   show_tasks_button.grid_remove()
+   schedule_task_button.grid_remove()
+   exit_button.grid_remove()
+   mount_back_button(action)
+
 
 root.mainloop()
 
